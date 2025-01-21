8d8b3ac06e7fcf5eeb687db66dc72e1a2e846703
DevPBDias <devpbdias@gmail.com>
2025-01-21 20:53:25 -0300

    feat: create  context-providers and dark theme button

---
+++ context/modal-provider.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+
+interface ModalProps {
+  callModal: boolean;
+  setCallModal: React.Dispatch<React.SetStateAction<boolean>>;
+  accountModal: boolean;
+  setAccountModal: React.Dispatch<React.SetStateAction<boolean>>;
+  callDeleteProject: boolean;
+  setCallDeleteProject: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const ModalContext = createContext<ModalProps | undefined>(undefined);
+
+export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
+  const [callModal, setCallModal] = useState(false);
+  const [accountModal, setAccountModal] = useState(false);
+  const [callDeleteProject, setCallDeleteProject] = useState(false);
+
+  return (
+    <ModalContext.Provider
+      value={{
+        callModal,
+        setCallModal,
+        accountModal,
+        setAccountModal,
+        callDeleteProject,
+        setCallDeleteProject,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModalContext = (): ModalProps => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("ModalContext deve ser usado dentro de um ModalProvider");
+  }
+  return context;
+};
