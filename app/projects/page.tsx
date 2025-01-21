55ea96c6f27499417464f4f5d3bc2b823a992efb
DevPBDias <devpbdias@gmail.com>
2025-01-21 20:47:12 -0300

    feat: create page and layout for projects page and add logos

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import { SettingsProjectModal } from "@/components/modals/projects";
+
+const HomePage = () => (
+  <main>
+    Hello World
+    <SettingsProjectModal />
+  </main>
+);
+
+export default HomePage;
