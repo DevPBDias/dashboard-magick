3483ba127f17d795dc3da3e07c42a921a6eae2f5
DevPBDias <devpbdias@gmail.com>
2025-01-31 18:28:32 -0300

    feat: add responsive css to account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -7,11 +7,11 @@ import React from "react";
 
 const AccountPage = () => {
   return (
-    <main className="bg-sidebar m-4 p-4 rounded-md h-full flex flex-col justify-start items-start gap-8 dark:bg-muted/50">
-      <h3 className="font-bold text-2xl flex justify-center items-center gap-2">
+    <main className="bg-sidebar p-3 border-t h-full flex flex-col justify-start items-start gap-8 dark:bg-muted/50">
+      <h3 className="font-bold text-xl flex justify-center items-center gap-2">
         <HandMetal /> Username
       </h3>
-      <section className="grid w-full grid-cols-3 grid-rows-1 gap-3">
+      <section className="grid w-full grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 2xl:grid-cols-3 2xl:grid-rows-3  gap-3">
         <UserCard />
         <SessionCard />
         <BillingCard />
